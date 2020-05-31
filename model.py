2870f8bf14f8d5da36b26109b516a300dab92174
kajrud <kaja.kinga.rudnicka@gmail.com>
2020-05-31 17:18:21 +0200

    31/05

---
+++ model.py
@@ -0,0 +1,32 @@
+# Class model for a meal planner
+
+
+class Meal ():
+    def __init__(self, name, ingredients):
+        self.name = name
+        self.ingredients = ingredients
+
+    def __str__(self):
+        return f"Aby wykonać {self.name}, musisz kupić {self.ingredients}"
+
+
+class Dinner(Meal):
+    def __init__(self, name, ingredients):
+        super().__init__(name, ingredients)
+        self.tags = ['dinner', 'lunch']
+
+
+class Non_dinner(Meal):
+    def __init__(self, name, ingredients):
+        super().__init__(name, ingredients)
+        self.tags = ['breakfast', 'brunch', 'supper']
+
+
+class Snack (Meal):
+    def __init__(self, name, ingredients):
+        super().__init__(name, ingredients)
+        self.tags = ['snack', 'appetizer', 'starter', 'between meals']
+
+
+scrambled_eggs = Non_dinner('jajecznica', ['jajko', 'masło', 'bekon'])
+print(scrambled_eggs)
